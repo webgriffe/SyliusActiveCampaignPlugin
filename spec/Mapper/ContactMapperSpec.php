c868a61156f923bae419b8a094703c608b3dd589
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-07 17:56:39 +0100

    Return an instance of ActiveCampaignContact on ContactMapper (#3)

--- spec/Mapper/ContactMapperSpec.php
+++ spec/Mapper/ContactMapperSpec.php
@@ -5,14 +5,26 @@ declare(strict_types=1);
 namespace spec\Webgriffe\SyliusActiveCampaignPlugin\Mapper;
 
 use PhpSpec\ObjectBehavior;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Exception\CustomerDoesNotHaveEmailException;
+use Webgriffe\SyliusActiveCampaignPlugin\Factory\ActiveCampaignContactFactoryInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Mapper\ContactMapper;
 use Webgriffe\SyliusActiveCampaignPlugin\Mapper\ContactMapperInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignContact;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignContactInterface;
 
 class ContactMapperSpec extends ObjectBehavior
 {
-    public function let(): void
-    {
-        $this->beConstructedWith();
+    public function let(
+        ActiveCampaignContactFactoryInterface $contactFactory,
+        CustomerInterface $customer,
+        ActiveCampaignContactInterface $contact
+    ): void {
+        $customer->getEmail()->willReturn('customer@domain.org');
+
+        $contactFactory->createNewFromEmail('customer@domain.org')->willReturn($contact);
+
+        $this->beConstructedWith($contactFactory);
     }
 
     public function it_is_initializable(): void
@@ -23,5 +35,20 @@ class ContactMapperSpec extends ObjectBehavior
     public function it_implements_contact_mapper_interface(): void
     {
         $this->shouldImplement(ContactMapperInterface::class);
+    }
+
+    public function it_throws_if_customer_does_not_have_email(CustomerInterface $customer): void
+    {
+        $customer->getId()->shouldBeCalledOnce()->willReturn(1);
+        $customer->getEmail()->willReturn(null);
+
+        $this->shouldThrow(CustomerDoesNotHaveEmailException::class)->during(
+            'mapFromCustomer', [$customer]
+        );
+    }
+
+    public function it_returns_an_instance_of_active_campaign_contact(CustomerInterface $customer,): void
+    {
+        $this->mapFromCustomer($customer)->shouldReturnAnInstanceOf(ActiveCampaignContactInterface::class);
     }
 }
--- src/Mapper/ContactMapper.php
+++ src/Mapper/ContactMapper.php
@@ -5,12 +5,27 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusActiveCampaignPlugin\Mapper;
 
 use Sylius\Component\Core\Model\CustomerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Exception\CustomerDoesNotHaveEmailException;
+use Webgriffe\SyliusActiveCampaignPlugin\Factory\ActiveCampaignContactFactoryInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignContactInterface;
 
 final class ContactMapper implements ContactMapperInterface
 {
+    public function __construct(
+        private ActiveCampaignContactFactoryInterface $contactFactory
+    ) {
+    }
+
     public function mapFromCustomer(CustomerInterface $customer): ActiveCampaignContactInterface
     {
-        throw new \RuntimeException('TODO');
+        $customerEmail = $customer->getEmail();
+        if ($customerEmail === null) {
+            throw new CustomerDoesNotHaveEmailException(sprintf(
+                'Unable to create a new ActiveCampaign Contact, the customer "%s" does not have a valid email.',
+                (string) $customer->getId()
+            ));
+        }
+
+        return $this->contactFactory->createNewFromEmail($customerEmail);
     }
 }
