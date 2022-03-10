93faa6e17ee1dc4e03a27040f524000009e2d8b7
Luca Gallinari <luke.gallinari@gmail.com>
2022-03-10 17:04:53 +0100

    Add some asserts for psalm (#3)

--- src/Factory/ActiveCampaignContact/CreateContactResponseFactory.php
+++ src/Factory/ActiveCampaignContact/CreateContactResponseFactory.php
@@ -6,20 +6,31 @@ namespace Webgriffe\SyliusActiveCampaignPlugin\Factory\ActiveCampaignContact;
 
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\CreateContactResponse;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\CreateContactResponseInterface;
+use Webmozart\Assert\Assert;
 
 final class CreateContactResponseFactory implements CreateContactResponseFactoryInterface
 {
     public function createNewFromPayload(array $payload): CreateContactResponseInterface
     {
+        // is there any better way to do this?
+        Assert::isArray($payload['fieldValues']);
         $fieldValues = $payload['fieldValues'];
+        Assert::string($payload['email']);
         $email = $payload['email'];
+        Assert::string($payload['cdate']);
         $createdAt = $payload['cdate'];
+        Assert::string($payload['udate']);
         $updatedAt = $payload['udate'];
+        Assert::string($payload['origid']);
         $organizationId = $payload['origid'];
+        Assert::string($payload['organization']);
         $organization = $payload['organization'];
+        Assert::isArray($payload['links']);
         $links = $payload['links'];
+        Assert::string($payload['id']);
         $id = $payload['id'];
 
+        /** @psalm-suppress MixedArgumentTypeCoercion */
         return new CreateContactResponse($fieldValues, $email, $createdAt, $updatedAt, $organizationId, $links, $id, $organization);
     }
 }
