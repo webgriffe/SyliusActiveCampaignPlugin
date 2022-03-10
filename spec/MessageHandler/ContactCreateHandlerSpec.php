1f8a437532a4b1a7dd31db36f883e30ea31664b9
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-10 15:36:35 +0100

    Call createContact on CreateContactHandler (#3)

--- spec/MessageHandler/ContactCreateHandlerSpec.php
+++ spec/MessageHandler/ContactCreateHandlerSpec.php
@@ -4,26 +4,75 @@ declare(strict_types=1);
 
 namespace spec\Webgriffe\SyliusActiveCampaignPlugin\MessageHandler;
 
+use InvalidArgumentException;
 use PhpSpec\ObjectBehavior;
-use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Model\CustomerInterface as SyliusCustomerInterface;
+use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignClientInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Mapper\ContactMapperInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\ContactCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\ContactCreateHandler;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ContactInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+
+interface CustomerInterface extends SyliusCustomerInterface, ActiveCampaignAwareInterface
+{
+}
 
 class ContactCreateHandlerSpec extends ObjectBehavior
 {
     public function let(
         ContactMapperInterface $contactMapper,
         ContactInterface $contact,
-        CustomerInterface $customer
+        CustomerInterface $customer,
+        ActiveCampaignClientInterface $activeCampaignClient,
+        CustomerRepositoryInterface $customerRepository
     ): void {
         $contactMapper->mapFromCustomer($customer)->willReturn($contact);
 
-        $this->beConstructedWith($contactMapper);
+        $customerRepository->find(12)->willReturn($customer);
+
+        $this->beConstructedWith($contactMapper, $activeCampaignClient, $customerRepository);
     }
 
     public function it_is_initializable(): void
     {
         $this->shouldBeAnInstanceOf(ContactCreateHandler::class);
+    }
+
+    public function it_throws_if_customer_is_not_found(
+        CustomerRepositoryInterface $customerRepository
+    ): void {
+        $customerRepository->find(12)->shouldBeCalledOnce()->willReturn(null);
+        $this->shouldThrow(InvalidArgumentException::class)->during(
+            '__invoke', [new ContactCreate(12)]
+        );
+    }
+
+    public function it_throws_if_customer_is_not_an_implementation_of_active_campaign_aware_interface(
+        CustomerRepositoryInterface $customerRepository,
+        SyliusCustomerInterface $syliusCustomer
+    ): void {
+        $customerRepository->find(12)->shouldBeCalledOnce()->willReturn($syliusCustomer);
+        $this->shouldThrow(InvalidArgumentException::class)->during(
+            '__invoke', [new ContactCreate(12)]
+        );
+    }
+
+    public function it_throws_if_customer_has_been_already_exported_to_active_campaign(
+        ActiveCampaignAwareInterface $customer
+    ): void {
+        $customer->getActiveCampaignId()->willReturn('321');
+        $this->shouldThrow(InvalidArgumentException::class)->during(
+            '__invoke', [new ContactCreate(12)]
+        );
+    }
+
+    public function it_creates_contact_on_active_campaign(
+        ContactInterface $contact,
+        ActiveCampaignClientInterface $activeCampaignClient,
+    ): void {
+        $activeCampaignClient->createContact($contact)->shouldBeCalledOnce()->willReturn();
+        $this->__invoke(new ContactCreate(12));
     }
 }
--- src/MessageHandler/ContactCreateHandler.php
+++ src/MessageHandler/ContactCreateHandler.php
@@ -23,20 +23,19 @@ final class ContactCreateHandler
 
     public function __invoke(ContactCreate $message): void
     {
+        $customerId = $message->getCustomerId();
         /** @var CustomerInterface|null $customer */
-        $customer = $this->customerRepository->find($message->getCustomerId());
+        $customer = $this->customerRepository->find($customerId);
         if ($customer === null) {
-            //TODO: Delete the customer?
-            return;
+            throw new InvalidArgumentException(sprintf('Customer with id "%s" does not exists', $customerId));
         }
         if (!$customer instanceof ActiveCampaignAwareInterface) {
             throw new InvalidArgumentException(sprintf('The Customer entity should implement the "%s" class', ActiveCampaignAwareInterface::class));
         }
 
-        if ($customer->getActiveCampaignId() !== null) {
-            //TODO: Throw?
-
-            return;
+        $activeCampaignId = $customer->getActiveCampaignId();
+        if ($activeCampaignId !== null) {
+            throw new InvalidArgumentException(sprintf('The customer with id "%s" has been already created on ActiveCampaign on the contact with id "%s"', $customerId, $activeCampaignId));
         }
         $this->activeCampaignClient->createContact($this->contactMapper->mapFromCustomer($customer));
     }
