45140abce247689726316e4733ef5f902639615b
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-10 17:56:13 +0100

    Fix Phpunit (#3)

--- tests/Client/ActiveCampaignClientStub.php
+++ tests/Client/ActiveCampaignClientStub.php
@@ -6,16 +6,16 @@ namespace Tests\Webgriffe\SyliusActiveCampaignPlugin\Client;
 
 use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignClientInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ContactInterface;
-use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ContactResponse;
-use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ContactResponseInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\CreateContactResponse;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\CreateContactResponseInterface;
 
 final class ActiveCampaignClientStub implements ActiveCampaignClientInterface
 {
     public string $activeCampaignId = '1234';
 
-    public function createContact(ContactInterface $contact): ContactResponseInterface
+    public function createContact(ContactInterface $contact): CreateContactResponseInterface
     {
-        return new ContactResponse(
+        return new CreateContactResponse(
             [],
             $contact->getEmail(),
             (new \DateTimeImmutable('now'))->format('c'),
