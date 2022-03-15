308966eae39d8300fc90c6932e434259a47b1ee8
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-15 14:46:51 +0100

    Add ContactUpdate subscriber (#3)

--- src/EventSubscriber/CustomerSubscriber.php
+++ src/EventSubscriber/CustomerSubscriber.php
@@ -9,6 +9,8 @@ use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\ContactCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\ContactUpdate;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
 
 final class CustomerSubscriber implements EventSubscriberInterface
 {
@@ -21,6 +23,7 @@ final class CustomerSubscriber implements EventSubscriberInterface
     {
         return [
             'sylius.customer.post_create' => ['createContact'],
+            'sylius.customer.post_update' => ['updateContact'],
         ];
     }
 
@@ -40,5 +43,27 @@ final class CustomerSubscriber implements EventSubscriberInterface
         }
 
         $this->messageBus->dispatch(new ContactCreate($customerId));
+    }
+
+    public function updateContact(GenericEvent $event): void
+    {
+        $customer = $event->getSubject();
+        if (!$customer instanceof CustomerInterface || !$customer instanceof ActiveCampaignAwareInterface) {
+            return;
+        }
+        /** @var mixed $customerId */
+        $customerId = $customer->getId();
+        if ($customerId === null) {
+            return;
+        }
+        if (!is_int($customerId)) {
+            $customerId = (string) $customerId;
+        }
+        $activeCampaignId = $customer->getActiveCampaignId();
+        if ($activeCampaignId === null) {
+            return;
+        }
+
+        $this->messageBus->dispatch(new ContactUpdate($customerId, $activeCampaignId));
     }
 }
--- tests/Integration/EventSubscriber/CustomerSubscriberTest.php
+++ tests/Integration/EventSubscriber/CustomerSubscriberTest.php
@@ -10,6 +10,7 @@ use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Transport\InMemoryTransport;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\ContactCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\ContactUpdate;
 
 final class CustomerSubscriberTest extends AbstractEventDispatcherTest
 {
@@ -35,5 +36,23 @@ final class CustomerSubscriberTest extends AbstractEventDispatcherTest
         $message = $messages[0];
         $this->assertInstanceOf(ContactCreate::class, $message->getMessage());
         $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+    }
+
+    public function test_that_it_updates_contact_on_active_campaign(): void
+    {
+        $customer = new Customer();
+        $customer->setEmail('info@activecampaign.com');
+        $customer->setActiveCampaignId(1234);
+        $this->customerRepository->add($customer);
+        $this->eventDispatcher->dispatch(new ResourceControllerEvent($customer), 'sylius.customer.post_update');
+        /** @var InMemoryTransport $transport */
+        $transport = self::getContainer()->get('messenger.transport.main');
+        /** @var Envelope[] $messages */
+        $messages = $transport->get();
+        $this->assertCount(1, $messages);
+        $message = $messages[0];
+        $this->assertInstanceOf(ContactUpdate::class, $message->getMessage());
+        $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
     }
 }
