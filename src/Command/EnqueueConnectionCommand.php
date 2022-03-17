f4e6ef08774a2d1563cf26044ee41c731696e1c9
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-17 17:48:39 +0100

    Add EnqueueConnectionsCommand to enqueue all channels (#10)

--- src/Command/EnqueueConnectionCommand.php
+++ src/Command/EnqueueConnectionCommand.php
@@ -5,7 +5,7 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusActiveCampaignPlugin\Command;
 
 use InvalidArgumentException;
-use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Symfony\Component\Console\Command\Command;
 use Symfony\Component\Console\Command\LockableTrait;
 use Symfony\Component\Console\Helper\ProgressBar;
@@ -16,26 +16,26 @@ use Symfony\Component\Console\Output\OutputInterface;
 use Symfony\Component\Console\Style\SymfonyStyle;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Symfony\Component\Stopwatch\Stopwatch;
-use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Connection\ConnectionCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Repository\ActiveCampaignAwareRepositoryInterface;
 use Webmozart\Assert\Assert;
 
-final class EnqueueContactCommand extends Command
+final class EnqueueConnectionCommand extends Command
 {
     use LockableTrait;
 
-    private const CUSTOMER_ID_ARGUMENT_CODE = 'customer-id';
+    private const CHANNEL_ID_ARGUMENT_CODE = 'channel-id';
 
-    public const ENQUEUE_CONTACT_COMMAND_STOPWATCH_NAME = 'enqueue-contact-command';
+    public const ENQUEUE_CONNNECTION_COMMAND_STOPWATCH_NAME = 'enqueue-connection-command';
 
     public const ALL_OPTION_CODE = 'all';
 
     /** @psalm-suppress PropertyNotSetInConstructor */
     private SymfonyStyle $io;
 
-    /** @param ActiveCampaignAwareRepositoryInterface<CustomerInterface> $customerRepository */
+    /** @param ActiveCampaignAwareRepositoryInterface<ChannelInterface> $channelRepository */
     public function __construct(
-        private ActiveCampaignAwareRepositoryInterface $customerRepository,
+        private ActiveCampaignAwareRepositoryInterface $channelRepository,
         private MessageBusInterface $messageBus,
         private ?string $name = null
     ) {
@@ -46,8 +46,8 @@ final class EnqueueContactCommand extends Command
     {
         $this
             ->setHelp($this->getCommandHelp())
-            ->addArgument(self::CUSTOMER_ID_ARGUMENT_CODE, InputArgument::OPTIONAL, 'The identifier id of the customer to enqueue.')
-            ->addOption(self::ALL_OPTION_CODE, 'a', InputOption::VALUE_NONE, 'If set, the command will enqueue all the customers without an ActiveCampaign\'s id.')
+            ->addArgument(self::CHANNEL_ID_ARGUMENT_CODE, InputArgument::OPTIONAL, 'The identifier id of the channel to enqueue.')
+            ->addOption(self::ALL_OPTION_CODE, 'a', InputOption::VALUE_NONE, 'If set, the command will enqueue all the channels without an ActiveCampaign\'s id.')
         ;
     }
 
@@ -58,27 +58,27 @@ final class EnqueueContactCommand extends Command
 
     protected function interact(InputInterface $input, OutputInterface $output): void
     {
-        if ($input->getOption(self::ALL_OPTION_CODE) === true || $input->getArgument(self::CUSTOMER_ID_ARGUMENT_CODE) !== null) {
+        if ($input->getOption(self::ALL_OPTION_CODE) === true || $input->getArgument(self::CHANNEL_ID_ARGUMENT_CODE) !== null) {
             return;
         }
 
-        $this->io->title('Enqueue Contact Command Interactive Wizard');
+        $this->io->title('Enqueue Connection Command Interactive Wizard');
         $this->io->text([
             'If you prefer to not use this interactive wizard, provide the',
             'argument required by this command as follows:',
             '',
-            ' $ php bin/console ' . (string) $this->name . ' customer-id',
+            ' $ php bin/console ' . (string) $this->name . ' channel-id',
             '',
             'Now we\'ll ask you for the value of the customer to enqueue.',
         ]);
 
-        // Ask for the Customer ID if it's not defined
-        /** @var mixed|null $customerId */
-        $customerId = $input->getArgument(self::CUSTOMER_ID_ARGUMENT_CODE);
-        if (null === $customerId) {
-            /** @var mixed $customerId */
-            $customerId = $this->io->ask('Customer id', null, [$this, 'validateCustomerId']);
-            $input->setArgument(self::CUSTOMER_ID_ARGUMENT_CODE, $customerId);
+        // Ask for the Channel ID if it's not defined
+        /** @var mixed|null $channelId */
+        $channelId = $input->getArgument(self::CHANNEL_ID_ARGUMENT_CODE);
+        if (null === $channelId) {
+            /** @var mixed $channelId */
+            $channelId = $this->io->ask('Channel id', null, [$this, 'validateChannelId']);
+            $input->setArgument(self::CHANNEL_ID_ARGUMENT_CODE, $channelId);
         }
     }
 
@@ -90,28 +90,28 @@ final class EnqueueContactCommand extends Command
             return Command::FAILURE;
         }
         $stopwatch = new Stopwatch();
-        $stopwatch->start(self::ENQUEUE_CONTACT_COMMAND_STOPWATCH_NAME);
+        $stopwatch->start(self::ENQUEUE_CONNNECTION_COMMAND_STOPWATCH_NAME);
 
-        /** @var string|int $customerId */
-        $customerId = $input->getArgument(self::CUSTOMER_ID_ARGUMENT_CODE);
+        /** @var string|int $channelId */
+        $channelId = $input->getArgument(self::CHANNEL_ID_ARGUMENT_CODE);
         $exportAll = (bool) $input->getOption(self::ALL_OPTION_CODE);
 
-        $this->validateInputData($customerId, $exportAll);
+        $this->validateInputData($channelId, $exportAll);
 
         if ($exportAll) {
-            $customersToExport = $this->customerRepository->findAllToEnqueue();
+            $channelsToExport = $this->channelRepository->findAllToEnqueue();
         } else {
-            $customer = $this->customerRepository->findOneToEnqueue($customerId);
-            if ($customer === null) {
+            $channel = $this->channelRepository->findOneToEnqueue($channelId);
+            if ($channel === null) {
                 throw new InvalidArgumentException(sprintf(
-                    'Unable to find a Customer with id "%s".',
-                    $customerId
+                    'Unable to find a Channel with id "%s".',
+                    $channelId
                 ));
             }
-            $customersToExport = [$customer];
+            $channelsToExport = [$channel];
         }
 
-        $progressBar = new ProgressBar($output, count($customersToExport));
+        $progressBar = new ProgressBar($output, count($channelsToExport));
         $progressBar->setFormat(
             "<fg=white;bg=black> %status:-45s%</>\n%current%/%max% [%bar%] %percent:3s%%\n🏁  %estimated:-21s% %memory:21s%"
         );
@@ -119,21 +119,21 @@ final class EnqueueContactCommand extends Command
         $progressBar->setEmptyBarCharacter('<fg=blue>⚬</>');
         $progressBar->setProgressCharacter('🚀');
         $progressBar->setRedrawFrequency(10);
-        $progressBar->setMessage(sprintf('Starting the enqueue for %s customers...', count($customersToExport)), 'status');
+        $progressBar->setMessage(sprintf('Starting the enqueue for %s channels...', count($channelsToExport)), 'status');
         $progressBar->start();
 
-        foreach ($customersToExport as $customer) {
-            /** @var string|int|null $customerId */
-            $customerId = $customer->getId();
-            Assert::notNull($customerId);
-            $this->messageBus->dispatch(new ContactCreate($customerId));
-            $progressBar->setMessage(sprintf('Customer "%s" enqueued!', (string) $customer->getId()), 'status');
+        foreach ($channelsToExport as $channel) {
+            /** @var string|int|null $channelId */
+            $channelId = $channel->getId();
+            Assert::notNull($channelId);
+            $this->messageBus->dispatch(new ConnectionCreate($channelId));
+            $progressBar->setMessage(sprintf('Channel "%s" enqueued!', (string) $channel->getId()), 'status');
             $progressBar->advance();
         }
-        $progressBar->setMessage(sprintf('Finished to enqueue the %s customers 🎉', count($customersToExport)), 'status');
+        $progressBar->setMessage(sprintf('Finished to enqueue the %s channels 🎉', count($channelsToExport)), 'status');
         $progressBar->finish();
 
-        $event = $stopwatch->stop(self::ENQUEUE_CONTACT_COMMAND_STOPWATCH_NAME);
+        $event = $stopwatch->stop(self::ENQUEUE_CONNNECTION_COMMAND_STOPWATCH_NAME);
         $this->io->comment(sprintf('Elapsed time: %.2f ms / Consumed memory: %.2f MB', $event->getDuration(), $event->getMemory() / (1024 ** 2)));
 
         $this->release();
@@ -149,39 +149,39 @@ final class EnqueueContactCommand extends Command
         if ($all) {
             return;
         }
-        $this->validateCustomerId($customerId);
+        $this->validateChannelId($customerId);
     }
 
     /**
-     * @param string|int|null $customerId
+     * @param string|int|null $channelId
      *
      * @return string|int
      */
-    public function validateCustomerId($customerId)
+    public function validateChannelId($channelId)
     {
-        if ($customerId === null || $customerId === '') {
-            throw new InvalidArgumentException('The Customer id can not be empty.');
+        if ($channelId === null || $channelId === '') {
+            throw new InvalidArgumentException('The Channel id can not be empty.');
         }
 
-        return $customerId;
+        return $channelId;
     }
 
     private function getCommandHelp(): string
     {
         return <<<'HELP'
-            The <info>%command.name%</info> command enqueue all Sylius customers without an ActiveCampaign's id to export them on ActiveCampaign:
+            The <info>%command.name%</info> command enqueue all Sylius channels without an ActiveCampaign's id to export them on ActiveCampaign:
 
-              <info>php %command.full_name%</info> <comment>customer-id</comment>
+              <info>php %command.full_name%</info> <comment>channel-id</comment>
 
-            By default the command enqueue only one customer. To enqueue all customers,
+            By default the command enqueue only one channel. To enqueue all channels,
             add the <comment>--all</comment> option:
 
               <info>php %command.full_name%</info> <comment>--all</comment>
 
             If you omit the argument and the option, the command will ask you to
-            provide the missing customer id:
+            provide the missing channel id:
 
-              # command will ask you for the customer id
+              # command will ask you for the channel id
               <info>php %command.full_name%</info>
             HELP;
     }
---
+++ tests/Integration/Command/EnqueueConnectionCommandTest.php
@@ -0,0 +1,114 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Tests\Webgriffe\SyliusActiveCampaignPlugin\Integration\Command;
+
+use App\Entity\Channel\Channel;
+use Doctrine\Common\DataFixtures\Purger\ORMPurger;
+use Doctrine\ORM\EntityManagerInterface;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Currency\Model\Currency;
+use Sylius\Component\Locale\Model\Locale;
+use Symfony\Component\Messenger\Envelope;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Connection\ConnectionCreate;
+
+final class EnqueueConnectionCommandTest extends AbstractCommandTest
+{
+    private ChannelRepositoryInterface $channelRepository;
+
+    private Locale $locale;
+
+    private Currency $currency;
+
+    protected function setUp(): void
+    {
+        parent::setUp();
+        $entityManager = self::getContainer()->get('doctrine.orm.entity_manager');
+        $purger = new ORMPurger($entityManager);
+        $purger->purge();
+        $this->channelRepository = self::getContainer()->get('sylius.repository.channel');
+
+        $this->locale = new Locale();
+        $this->locale->setCode('en_US');
+        $entityManager->persist($this->locale);
+        $this->currency = new Currency();
+        $this->currency->setCode('EUR');
+        $entityManager->persist($this->currency);
+    }
+
+    public function test_that_it_enqueues_connection(): void
+    {
+        $channel = $this->createChannel();
+        self::assertNotNull($channel->getId());
+        $commandTester = $this->executeCommand([
+            'channel-id' => $channel->getId(),
+        ], []);
+        self::assertEquals(0, $commandTester->getStatusCode());
+
+        $transport = self::getContainer()->get('messenger.transport.main');
+        /** @var Envelope[] $messages */
+        $messages = $transport->get();
+        $this->assertCount(1, $messages);
+        $message = $messages[0];
+        $this->assertInstanceOf(ConnectionCreate::class, $message->getMessage());
+        $this->assertEquals($channel->getId(), $message->getMessage()->getChannelId());
+    }
+
+    public function test_that_it_enqueues_connection_interactively(): void
+    {
+        $channel = $this->createChannel();
+        self::assertNotNull($channel->getId());
+        $commandTester = $this->executeCommand([], [
+            $channel->getId(),
+        ]);
+        self::assertEquals(0, $commandTester->getStatusCode());
+
+        $transport = self::getContainer()->get('messenger.transport.main');
+        /** @var Envelope[] $messages */
+        $messages = $transport->get();
+        $this->assertCount(1, $messages);
+        $message = $messages[0];
+        $this->assertInstanceOf(ConnectionCreate::class, $message->getMessage());
+        $this->assertEquals($channel->getId(), $message->getMessage()->getChannelId());
+    }
+
+    public function test_that_it_enqueues_all_contacts(): void
+    {
+        $firstChannel = $this->createChannel('ecommerce');
+        $secondChannel = $this->createChannel('support');
+        $commandTester = $this->executeCommand([
+            '--all' => true,
+        ]);
+        self::assertEquals(0, $commandTester->getStatusCode());
+
+        $transport = self::getContainer()->get('messenger.transport.main');
+        /** @var Envelope[] $messages */
+        $messages = $transport->get();
+        $this->assertCount(2, $messages);
+        $message = $messages[0];
+        $this->assertInstanceOf(ConnectionCreate::class, $message->getMessage());
+        $this->assertEquals($firstChannel->getId(), $message->getMessage()->getChannelId());
+        $message = $messages[1];
+        $this->assertInstanceOf(ConnectionCreate::class, $message->getMessage());
+        $this->assertEquals($secondChannel->getId(), $message->getMessage()->getChannelId());
+    }
+
+    protected function getCommandDefinition(): string
+    {
+        return 'webgriffe.sylius_active_campaign_plugin.command.enqueue_connection';
+    }
+
+    private function createChannel(string $code = 'ecommerce'): Channel
+    {
+        $channel = new Channel();
+        $channel->setCode($code);
+        $channel->setName('E Commerce');
+        $channel->setTaxCalculationStrategy('order_items_based');
+        $channel->setDefaultLocale($this->locale);
+        $channel->setBaseCurrency($this->currency);
+        $this->channelRepository->add($channel);
+
+        return $channel;
+    }
+}
