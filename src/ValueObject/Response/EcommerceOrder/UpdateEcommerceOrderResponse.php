659da2035db011fcb993aaa8bfc5c75b527b6009
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-18 17:08:34 +0100

    Add Ecommerce Order client (#4)

---
+++ src/ValueObject/Response/EcommerceOrder/UpdateEcommerceOrderResponse.php
@@ -0,0 +1,11 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\EcommerceOrder;
+
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\UpdateResourceResponseInterface;
+
+final class UpdateEcommerceOrderResponse implements UpdateResourceResponseInterface
+{
+}
