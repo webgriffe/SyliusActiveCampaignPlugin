518a358d990d23e687dfdfc89c94b23bf78f9902
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-23 11:56:41 +0100

    Implements CustomerSubscriber with Ecommerce Customer events (#11)

--- src/EventSubscriber/CustomerSubscriber.php
+++ src/EventSubscriber/CustomerSubscriber.php
@@ -4,6 +4,8 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusActiveCampaignPlugin\EventSubscriber;
 
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
@@ -11,21 +13,25 @@ use Symfony\Component\Messenger\MessageBusInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactRemove;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactUpdate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerRemove;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerUpdate;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
 
 final class CustomerSubscriber implements EventSubscriberInterface
 {
     public function __construct(
-        private MessageBusInterface $messageBus
+        private MessageBusInterface $messageBus,
+        private ChannelRepositoryInterface $channelRepository
     ) {
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            'sylius.customer.post_create' => ['createContact'],
-            'sylius.customer.post_update' => ['updateContact'],
-            'sylius.customer.post_delete' => ['removeContact'],
+            'sylius.customer.post_create' => [['createContact'], ['createEcommerceCustomer']],
+            'sylius.customer.post_update' => [['updateContact'], ['updateEcommerceCustomer']],
+            'sylius.customer.post_delete' => [['removeContact'], ['removeEcommerceCustomer']],
         ];
     }
 
@@ -45,6 +51,34 @@ final class CustomerSubscriber implements EventSubscriberInterface
         }
 
         $this->messageBus->dispatch(new ContactCreate($customerId));
+    }
+
+    public function createEcommerceCustomer(GenericEvent $event): void
+    {
+        $customer = $event->getSubject();
+        if (!$customer instanceof CustomerInterface) {
+            return;
+        }
+        /** @var mixed $customerId */
+        $customerId = $customer->getId();
+        if ($customerId === null) {
+            return;
+        }
+        if (!is_int($customerId)) {
+            $customerId = (string) $customerId;
+        }
+        /** @var ChannelInterface $channel */
+        foreach ($this->channelRepository->findBy(['enabled' => true]) as $channel) {
+            /** @var mixed $channelId */
+            $channelId = $channel->getId();
+            if ($channelId === null) {
+                continue;
+            }
+            if (!is_int($channelId)) {
+                $channelId = (string) $channelId;
+            }
+            $this->messageBus->dispatch(new EcommerceCustomerCreate($customerId, $channelId));
+        }
     }
 
     public function updateContact(GenericEvent $event): void
@@ -69,6 +103,38 @@ final class CustomerSubscriber implements EventSubscriberInterface
         $this->messageBus->dispatch(new ContactUpdate($customerId, $activeCampaignId));
     }
 
+    public function updateEcommerceCustomer(GenericEvent $event): void
+    {
+        $customer = $event->getSubject();
+        if (!$customer instanceof CustomerInterface || !$customer instanceof ActiveCampaignAwareInterface) {
+            return;
+        }
+        /** @var mixed $customerId */
+        $customerId = $customer->getId();
+        if ($customerId === null) {
+            return;
+        }
+        if (!is_int($customerId)) {
+            $customerId = (string) $customerId;
+        }
+        $activeCampaignId = $customer->getActiveCampaignId();
+        if ($activeCampaignId === null) {
+            return;
+        }
+        /** @var ChannelInterface $channel */
+        foreach ($this->channelRepository->findBy(['enabled' => true]) as $channel) {
+            /** @var mixed $channelId */
+            $channelId = $channel->getId();
+            if ($channelId === null) {
+                continue;
+            }
+            if (!is_int($channelId)) {
+                $channelId = (string) $channelId;
+            }
+            $this->messageBus->dispatch(new EcommerceCustomerUpdate($customerId, $activeCampaignId, $channelId));
+        }
+    }
+
     public function removeContact(GenericEvent $event): void
     {
         $customer = $event->getSubject();
@@ -81,5 +147,21 @@ final class CustomerSubscriber implements EventSubscriberInterface
         }
 
         $this->messageBus->dispatch(new ContactRemove($activeCampaignId));
+    }
+
+    public function removeEcommerceCustomer(GenericEvent $event): void
+    {
+        $customer = $event->getSubject();
+        if (!$customer instanceof ActiveCampaignAwareInterface) {
+            return;
+        }
+        $activeCampaignId = $customer->getActiveCampaignId();
+        if ($activeCampaignId === null) {
+            return;
+        }
+        /** @var ChannelInterface $channel */
+        foreach ($this->channelRepository->findBy(['enabled' => true]) as $channel) {
+            $this->messageBus->dispatch(new EcommerceCustomerRemove($activeCampaignId));
+        }
     }
 }
--- tests/Integration/EventSubscriber/CustomerSubscriberTest.php
+++ tests/Integration/EventSubscriber/CustomerSubscriberTest.php
@@ -6,12 +6,16 @@ namespace Tests\Webgriffe\SyliusActiveCampaignPlugin\Integration\EventSubscriber
 
 use Fidry\AliceDataFixtures\Persistence\PurgeMode;
 use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Transport\InMemoryTransport;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactRemove;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactUpdate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerRemove;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerUpdate;
 
 final class CustomerSubscriberTest extends AbstractEventDispatcherTest
 {
@@ -19,47 +23,73 @@ final class CustomerSubscriberTest extends AbstractEventDispatcherTest
 
     private CustomerRepositoryInterface $customerRepository;
 
+    private ChannelRepositoryInterface $channelRepository;
+
     protected function setUp(): void
     {
         parent::setUp();
         $this->customerRepository = self::getContainer()->get('sylius.repository.customer');
+        $this->channelRepository = self::getContainer()->get('sylius.repository.channel');
 
         $fixtureLoader = self::getContainer()->get('fidry_alice_data_fixtures.loader.doctrine');
         $fixtureLoader->load([
+            self::FIXTURE_BASE_DIR . '/channels.yaml',
             self::FIXTURE_BASE_DIR . '/customers.yaml',
         ], [], [], PurgeMode::createDeleteMode());
     }
 
-    public function test_that_it_creates_contact_on_active_campaign(): void
+    public function test_that_it_creates_contact_and_ecommerce_customer_on_active_campaign(): void
     {
         $customer = $this->customerRepository->findOneBy(['email' => 'jim@email.com']);
+        $fashionChannel = $this->channelRepository->findOneBy(['code' => 'fashion_shop']);
+        $digitalChannel = $this->channelRepository->findOneBy(['code' => 'digital_shop']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($customer), 'sylius.customer.post_create');
         /** @var InMemoryTransport $transport */
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+        $this->assertCount(3, $messages);
         $message = $messages[0];
         $this->assertInstanceOf(ContactCreate::class, $message->getMessage());
         $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerCreate::class, $message->getMessage());
+        $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($fashionChannel->getId(), $message->getMessage()->getChannelId());
+        $message = $messages[2];
+        $this->assertInstanceOf(EcommerceCustomerCreate::class, $message->getMessage());
+        $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($digitalChannel->getId(), $message->getMessage()->getChannelId());
     }
 
-    public function test_that_it_updates_contact_on_active_campaign(): void
+    public function test_that_it_updates_contact_and_ecommerce_customer__on_active_campaign(): void
     {
         $customer = $this->customerRepository->findOneBy(['email' => 'jim@email.com']);
+        $fashionChannel = $this->channelRepository->findOneBy(['code' => 'fashion_shop']);
+        $digitalChannel = $this->channelRepository->findOneBy(['code' => 'digital_shop']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($customer), 'sylius.customer.post_update');
         /** @var InMemoryTransport $transport */
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+        $this->assertCount(3, $messages);
         $message = $messages[0];
         $this->assertInstanceOf(ContactUpdate::class, $message->getMessage());
         $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
         $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerUpdate::class, $message->getMessage());
+        $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
+        $this->assertEquals($fashionChannel->getId(), $message->getMessage()->getChannelId());
+        $message = $messages[2];
+        $this->assertInstanceOf(EcommerceCustomerUpdate::class, $message->getMessage());
+        $this->assertEquals($customer->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
+        $this->assertEquals($digitalChannel->getId(), $message->getMessage()->getChannelId());
     }
 
-    public function test_that_it_removes_contact_on_active_campaign(): void
+    public function test_that_it_removes_contact_and_ecommerce_customer__on_active_campaign(): void
     {
         $customer = $this->customerRepository->findOneBy(['email' => 'jim@email.com']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($customer), 'sylius.customer.post_delete');
@@ -67,9 +97,15 @@ final class CustomerSubscriberTest extends AbstractEventDispatcherTest
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+        $this->assertCount(3, $messages);
         $message = $messages[0];
         $this->assertInstanceOf(ContactRemove::class, $message->getMessage());
+        $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerRemove::class, $message->getMessage());
+        $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
+        $message = $messages[2];
+        $this->assertInstanceOf(EcommerceCustomerRemove::class, $message->getMessage());
         $this->assertEquals($customer->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
     }
 }
