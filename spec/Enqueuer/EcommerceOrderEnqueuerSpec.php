1b3d7e321e6ed1be239208ae4d220c4cf6dd7a12
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-03-30 10:07:17 +0200

    Search for already existing order and cart on ActiveCampaign (#16)

--- spec/Enqueuer/EcommerceOrderEnqueuerSpec.php
+++ spec/Enqueuer/EcommerceOrderEnqueuerSpec.php
@@ -4,17 +4,23 @@ declare(strict_types=1);
 
 namespace spec\Webgriffe\SyliusActiveCampaignPlugin\Enqueuer;
 
+use App\Entity\Channel\ChannelInterface;
 use App\Entity\Order\OrderInterface;
 use Doctrine\ORM\EntityManagerInterface;
 use PhpSpec\ObjectBehavior;
 use Prophecy\Argument;
+use Sylius\Component\Core\Model\ChannelInterface as SyliusChannelInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\EcommerceOrderEnqueuer;
 use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\EcommerceOrderEnqueuerInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderRemove;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderUpdate;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\EcommerceOrder\EcommerceOrderResponse;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\ListResourcesResponseInterface;
 use Webmozart\Assert\InvalidArgumentException;
 
 class EcommerceOrderEnqueuerSpec extends ObjectBehavior
@@ -22,13 +28,18 @@ class EcommerceOrderEnqueuerSpec extends ObjectBehavior
     public function let(
         MessageBusInterface $messageBus,
         EntityManagerInterface $entityManager,
-        OrderInterface $order
+        ActiveCampaignResourceClientInterface $activeCampaignEcommerceOrderClient,
+        OrderInterface $order,
+        ChannelInterface $channel
     ): void {
         $order->getId()->willReturn(1);
         $order->getActiveCampaignId()->willReturn(null);
         $order->getState()->willReturn(OrderInterface::STATE_NEW);
+        $order->getChannel()->willReturn($channel);
 
-        $this->beConstructedWith($messageBus, $entityManager);
+        $channel->getActiveCampaignId()->willReturn(153);
+
+        $this->beConstructedWith($messageBus, $entityManager, $activeCampaignEcommerceOrderClient);
     }
 
     public function it_is_initializable(): void
@@ -72,13 +83,115 @@ class EcommerceOrderEnqueuerSpec extends ObjectBehavior
         $this->enqueue($order);
     }
 
+    public function it_throws_if_order_channel_is_null(OrderInterface $order): void
+    {
+        $order->getChannel()->willReturn(null);
+        $this->shouldThrow(new InvalidArgumentException('The order channel should implements "Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface"'))
+            ->during('enqueue', [$order]);
+    }
+
+    public function it_throws_if_order_channel_is_not_an_instance_of_active_campaign_aware(OrderInterface $order, SyliusChannelInterface $syliusChannel): void
+    {
+        $order->getChannel()->willReturn($syliusChannel);
+        $this->shouldThrow(new InvalidArgumentException('The order channel should implements "Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface"'))
+            ->during('enqueue', [$order]);
+    }
+
+    public function it_throws_if_order_channel_active_campaign_id_is_null(OrderInterface $order, ActiveCampaignAwareInterface $channel): void
+    {
+        $channel->getActiveCampaignId()->willReturn(null);
+        $this->shouldThrow(new InvalidArgumentException('The channel ActiveCampaign connection id should not be null'))
+            ->during('enqueue', [$order]);
+    }
+
+    public function it_enqueues_a_ecommerce_order_remove_if_order_ecommerce_order_active_campaign_id_is_null_and_state_is_canceled_and_it_is_found_on_active_campaign(
+        OrderInterface $order,
+        MessageBusInterface $messageBus,
+        EntityManagerInterface $entityManager,
+        ActiveCampaignResourceClientInterface $activeCampaignEcommerceOrderClient,
+        ListResourcesResponseInterface $listResourcesResponse
+    ): void {
+        $order->getState()->willReturn(OrderInterface::STATE_CANCELLED);
+        $order->setActiveCampaignId(null)->shouldNotBeCalled();
+        $entityManager->flush()->shouldNotBeCalled();
+
+        $activeCampaignEcommerceOrderClient->list([
+            'filters[connectionid]' => 153,
+            'filters[externalid]' => 1,
+        ])->shouldBeCalledOnce()->willReturn($listResourcesResponse);
+
+        $listResourcesResponse->getResourceResponseLists()->shouldBeCalledOnce()->willReturn([
+            new EcommerceOrderResponse(14)
+        ]);
+
+        $messageBus->dispatch(Argument::type(EcommerceOrderRemove::class))->shouldBeCalledOnce()->willReturn(new Envelope(new EcommerceOrderRemove(14)));
+
+        $this->enqueue($order);
+    }
+
+    public function it_enqueues_a_ecommerce_order_update_if_order_ecommerce_order_active_campaign_id_is_null_and_state_is_not_canceled_and_it_is_found_on_active_campaign(
+        OrderInterface $order,
+        MessageBusInterface $messageBus,
+        EntityManagerInterface $entityManager,
+        ActiveCampaignResourceClientInterface $activeCampaignEcommerceOrderClient,
+        ListResourcesResponseInterface $listResourcesResponse
+    ): void {
+        $order->getState()->willReturn(OrderInterface::STATE_NEW);
+        $order->setActiveCampaignId(14)->shouldBeCalledOnce();
+        $entityManager->flush()->shouldBeCalledOnce();
+
+        $activeCampaignEcommerceOrderClient->list([
+            'filters[connectionid]' => 153,
+            'filters[externalid]' => 1,
+        ])->shouldBeCalledOnce()->willReturn($listResourcesResponse);
+
+        $listResourcesResponse->getResourceResponseLists()->shouldBeCalledOnce()->willReturn([
+            new EcommerceOrderResponse(14),
+            new EcommerceOrderResponse(18),
+        ]);
+
+        $messageBus->dispatch(Argument::type(EcommerceOrderUpdate::class))->shouldBeCalledOnce()->willReturn(new Envelope(new EcommerceOrderUpdate(1,14, false)));
+
+        $this->enqueue($order, false);
+    }
+
+    public function it_search_for_external_checkout_id_if_order_state_is_cart(
+        OrderInterface $order,
+        MessageBusInterface $messageBus,
+        ActiveCampaignResourceClientInterface $activeCampaignEcommerceOrderClient,
+        ListResourcesResponseInterface $listResourcesResponse
+    ): void {
+        $order->getState()->willReturn(OrderInterface::STATE_CART);
+
+        $activeCampaignEcommerceOrderClient->list([
+            'filters[connectionid]' => 153,
+            'filters[externalcheckoutid]' => 1,
+        ])->shouldBeCalledOnce()->willReturn($listResourcesResponse);
+
+        $listResourcesResponse->getResourceResponseLists()->shouldBeCalledOnce()->willReturn([]);
+
+        $messageBus->dispatch(Argument::type(EcommerceOrderCreate::class))->shouldBeCalledOnce()->willReturn(new Envelope(new EcommerceOrderCreate(1)));
+
+        $this->enqueue($order);
+    }
+
     public function it_enqueues_a_ecommerce_order_create_if_order_ecommerce_order_active_campaign_id_is_null_and_state_is_not_canceled(
         OrderInterface $order,
         MessageBusInterface $messageBus,
-        EntityManagerInterface $entityManager
+        EntityManagerInterface $entityManager,
+        ActiveCampaignResourceClientInterface $activeCampaignEcommerceOrderClient,
+        ListResourcesResponseInterface $listResourcesResponse
     ): void {
         $order->setActiveCampaignId(null)->shouldNotBeCalled();
         $entityManager->flush()->shouldNotBeCalled();
+
+        $activeCampaignEcommerceOrderClient->list([
+            'filters[connectionid]' => 153,
+            'filters[externalid]' => 1,
+        ])->shouldBeCalledOnce()->willReturn($listResourcesResponse);
+
+        $listResourcesResponse->getResourceResponseLists()->shouldBeCalledOnce()->willReturn([]);
+
         $messageBus->dispatch(Argument::type(EcommerceOrderCreate::class))->shouldBeCalledOnce()->willReturn(new Envelope(new EcommerceOrderCreate(1)));
 
         $this->enqueue($order);
--- src/Enqueuer/EcommerceOrderEnqueuer.php
+++ src/Enqueuer/EcommerceOrderEnqueuer.php
@@ -7,16 +7,20 @@ namespace Webgriffe\SyliusActiveCampaignPlugin\Enqueuer;
 use Doctrine\ORM\EntityManagerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderRemove;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderUpdate;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\EcommerceOrder\EcommerceOrderResponse;
 use Webmozart\Assert\Assert;
 
 final class EcommerceOrderEnqueuer implements EcommerceOrderEnqueuerInterface
 {
     public function __construct(
         private MessageBusInterface $messageBus,
-        private EntityManagerInterface $entityManager
+        private EntityManagerInterface $entityManager,
+        private ActiveCampaignResourceClientInterface $activeCampaignEcommerceOrderClient
     ) {
     }
 
@@ -38,10 +42,35 @@ final class EcommerceOrderEnqueuer implements EcommerceOrderEnqueuerInterface
 
             return;
         }
+        /** @var ActiveCampaignAwareInterface|null $channel */
+        $channel = $order->getChannel();
+        Assert::isInstanceOf($channel, ActiveCampaignAwareInterface::class, sprintf('The order channel should implements "%s"', ActiveCampaignAwareInterface::class));
+        $channelActiveCampaignId = $channel->getActiveCampaignId();
+        Assert::notNull($channelActiveCampaignId, 'The channel ActiveCampaign connection id should not be null');
+        $ecommerceOrdersWithSameConnectionAndId = $this->activeCampaignEcommerceOrderClient->list([
+            'filters[connectionid]' => (string) $channelActiveCampaignId,
+            'filters[' . ($order->getState() === OrderInterface::STATE_CART ? 'externalcheckoutid' : 'externalid') . ']' => (string) $orderId,
+        ])->getResourceResponseLists();
+        if (count($ecommerceOrdersWithSameConnectionAndId) > 0) {
+            /** @var EcommerceOrderResponse $ecommerceOrder */
+            $ecommerceOrder = reset($ecommerceOrdersWithSameConnectionAndId);
+            $activeCampaignEcommerceOrderId = $ecommerceOrder->getId();
+
+            if ($order->getState() === OrderInterface::STATE_CANCELLED) {
+                $this->messageBus->dispatch(new EcommerceOrderRemove($activeCampaignEcommerceOrderId));
+
+                return;
+            }
+            $order->setActiveCampaignId($activeCampaignEcommerceOrderId);
+            $this->entityManager->flush();
+
+            $this->messageBus->dispatch(new EcommerceOrderUpdate($orderId, $activeCampaignEcommerceOrderId, $isInRealTime));
+
+            return;
+        }
         if ($order->getState() === OrderInterface::STATE_CANCELLED) {
             return;
         }
-        // TODO: Search for order number?
 
         $this->messageBus->dispatch(new EcommerceOrderCreate($orderId, $isInRealTime));
     }
