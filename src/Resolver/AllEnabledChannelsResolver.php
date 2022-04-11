2587b5d1f7d0027268fad61580eb9bc7c8b88d78
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-11 17:33:23 +0200

    Use findAllToEnqueue method on ChannelsResolver to use the same method of connection exporters (#36)

--- src/Resolver/AllEnabledChannelsResolver.php
+++ src/Resolver/AllEnabledChannelsResolver.php
@@ -4,26 +4,20 @@ declare(strict_types=1);
 
 namespace Webgriffe\SyliusActiveCampaignPlugin\Resolver;
 
-use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
 use Sylius\Component\Core\Model\ChannelInterface;
 use Sylius\Component\Core\Model\CustomerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Repository\ActiveCampaignResourceRepositoryInterface;
 use Webmozart\Assert\Assert;
 
 final class AllEnabledChannelsResolver implements CustomerChannelsResolverInterface
 {
-    private ChannelRepositoryInterface $channelRepository;
-
-    public function __construct(ChannelRepositoryInterface $channelRepository)
+    public function __construct(private ActiveCampaignResourceRepositoryInterface $channelRepository)
     {
-        $this->channelRepository = $channelRepository;
     }
 
-    /**
-     * @inheritDoc
-     */
     public function resolve(CustomerInterface $customer): array
     {
-        $channels = $this->channelRepository->findBy(['enabled' => true]);
+        $channels = $this->channelRepository->findAllToEnqueue();
         Assert::allIsInstanceOf($channels, ChannelInterface::class);
 
         return $channels;
