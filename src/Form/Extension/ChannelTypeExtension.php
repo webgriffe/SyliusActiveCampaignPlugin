35fbcb2f29c11ce477bf12abd8f7e68d87343ee9
Luca Gallinari <luke.gallinari@gmail.com>
2022-04-13 15:12:07 +0200

    Add form to edit the Channel list id (#41)

---
+++ src/Form/Extension/ChannelTypeExtension.php
@@ -0,0 +1,26 @@
+<?php
+
+declare(strict_types=1);
+
+namespace  Webgriffe\SyliusActiveCampaignPlugin\Form\Extension;
+
+use Sylius\Bundle\ChannelBundle\Form\Type\ChannelType;
+use Symfony\Component\Form\AbstractTypeExtension;
+use Symfony\Component\Form\Extension\Core\Type\TextType;
+use Symfony\Component\Form\FormBuilderInterface;
+
+final class ChannelTypeExtension extends AbstractTypeExtension
+{
+    public function buildForm(FormBuilderInterface $builder, array $options): void
+    {
+        $builder
+            ->add('activeCampaignListId', TextType::class, [
+                'label' => 'webgriffe_sylius_active_campaign.ui.list_id',
+            ]);
+    }
+
+    public static function getExtendedTypes(): iterable
+    {
+        return [ChannelType::class];
+    }
+}
