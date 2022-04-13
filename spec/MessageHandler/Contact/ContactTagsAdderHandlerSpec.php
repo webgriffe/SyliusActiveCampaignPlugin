a872f72c5d123ba561c3d36dc9ffa327e61620eb
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-13 16:25:21 +0200

    Send contact tags resolved (#40)

--- spec/MessageHandler/Contact/ContactTagsAdderHandlerSpec.php
+++ spec/MessageHandler/Contact/ContactTagsAdderHandlerSpec.php
@@ -6,14 +6,24 @@ namespace spec\Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact;
 
 use App\Entity\Customer\CustomerInterface;
 use InvalidArgumentException;
+use Psr\Log\LoggerInterface;
 use Sylius\Component\Core\Model\CustomerInterface as SyliusCustomerInterface;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
 use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Factory\ActiveCampaign\ContactTagFactoryInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Mapper\TagMapperInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactTagsAdder;
 use Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact\ContactTagsAdderHandler;
 use PhpSpec\ObjectBehavior;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ContactTagInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\TagInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Resolver\ContactTagsResolverInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\CreateResourceResponseInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\ListResourcesResponseInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\ResourceResponseInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\Tag\TagResponse;
 
 class ContactTagsAdderHandlerSpec extends ObjectBehavior
 {
@@ -22,12 +32,45 @@ class ContactTagsAdderHandlerSpec extends ObjectBehavior
         ActiveCampaignResourceClientInterface $activeCampaignContactTagClient,
         CustomerRepositoryInterface $customerRepository,
         ContactTagsResolverInterface $contactTagsResolver,
-        CustomerInterface $customer
-    ): void
-    {
+        TagMapperInterface $tagMapper,
+        ContactTagFactoryInterface $contactTagFactory,
+        LoggerInterface $logger,
+        CustomerInterface $customer,
+        ListResourcesResponseInterface $maleResourcesResponse,
+        ResourceResponseInterface $maleTagResponse,
+        ListResourcesResponseInterface $newResourcesResponse,
+        TagInterface $newTag,
+        CreateResourceResponseInterface $createResourceResponse,
+        ResourceResponseInterface $newTagResponse,
+        ContactTagInterface $maleContactTag,
+        ContactTagInterface $newContactTag,
+        CreateResourceResponseInterface $createMaleContactTagResourceResponse
+    ): void {
         $customerRepository->find(12)->willReturn($customer);
+        $customer->getActiveCampaignId()->willReturn(143);
 
-        $this->beConstructedWith($activeCampaignTagClient, $activeCampaignContactTagClient, $customerRepository, $contactTagsResolver);
+        $contactTagsResolver->resolve($customer)->willReturn(['male', 'new']);
+
+        $maleTagResponse->getId()->willReturn(4);
+        $maleResourcesResponse->getResourceResponseLists()->willReturn([$maleTagResponse]);
+        $activeCampaignTagClient->list(['search' => 'male'])->willReturn($maleResourcesResponse);
+
+        $newResourcesResponse->getResourceResponseLists()->willReturn([]);
+        $activeCampaignTagClient->list(['search' => 'new'])->willReturn($newResourcesResponse);
+
+        $tagMapper->mapFromTagAndTagType('new')->willReturn($newTag);
+
+        $newTagResponse->getId()->willReturn(7);
+        $createResourceResponse->getResourceResponse()->willReturn($newTagResponse);
+        $activeCampaignTagClient->create($newTag)->willReturn($createResourceResponse);
+
+        $contactTagFactory->createNew(143, 4)->willReturn($maleContactTag);
+        $contactTagFactory->createNew(143, 7)->willReturn($newContactTag);
+
+        $activeCampaignContactTagClient->create($maleContactTag)->willReturn($createMaleContactTagResourceResponse);
+        $activeCampaignContactTagClient->create($newContactTag)->willThrow(new HttpException(200));
+
+        $this->beConstructedWith($activeCampaignTagClient, $activeCampaignContactTagClient, $customerRepository, $contactTagsResolver, $tagMapper, $contactTagFactory, $logger);
     }
 
     public function it_is_initializable(): void
@@ -67,5 +110,24 @@ class ContactTagsAdderHandlerSpec extends ObjectBehavior
             '__invoke',
             [new ContactTagsAdder(12)]
         );
+    }
+
+    public function it_throws_if_adding_customer_tag_throws_not_200_or_201_response(
+        ContactTagFactoryInterface $contactTagFactory
+    ): void {
+        $contactTagFactory->createNew(143, 7)->willThrow(new HttpException(205));
+
+        $this->shouldThrow(new HttpException(205))->during(
+            '__invoke',
+            [new ContactTagsAdder(12)]
+        );
+    }
+
+    public function it_adds_contact_tags(
+        LoggerInterface $logger
+    ): void {
+        $logger->info('The tag with id "7" already exists for the contact with id "143".')->shouldBeCalledOnce();
+
+        $this->__invoke(new ContactTagsAdder(12));
     }
 }
