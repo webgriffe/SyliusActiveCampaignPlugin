b601ca57aac61fe942d4046511ff850373162172
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-13 14:39:22 +0200

    Add ContactTagsAdderHandler (#40)

---
+++ spec/MessageHandler/Contact/ContactTagsAdderHandlerSpec.php
@@ -0,0 +1,69 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact;
+
+use App\Entity\Customer\CustomerInterface;
+use InvalidArgumentException;
+use Sylius\Component\Core\Model\CustomerInterface as SyliusCustomerInterface;
+use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactTagsAdder;
+use Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact\ContactTagsAdderHandler;
+use PhpSpec\ObjectBehavior;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+
+class ContactTagsAdderHandlerSpec extends ObjectBehavior
+{
+    public function let(
+        ActiveCampaignResourceClientInterface $activeCampaignTagClient,
+        ActiveCampaignResourceClientInterface $activeCampaignContactTagClient,
+        CustomerRepositoryInterface $customerRepository,
+        CustomerInterface $customer
+    ): void
+    {
+        $customerRepository->find(12)->willReturn($customer);
+
+        $this->beConstructedWith($activeCampaignTagClient, $activeCampaignContactTagClient, $customerRepository);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldBeAnInstanceOf(ContactTagsAdderHandler::class);
+    }
+
+    public function it_throws_if_customer_is_not_found(
+        CustomerRepositoryInterface $customerRepository
+    ): void {
+        $customerRepository->find(12)->shouldBeCalledOnce()->willReturn(null);
+
+        $this->shouldThrow(new InvalidArgumentException('Customer with id "12" does not exists.'))->during(
+            '__invoke',
+            [new ContactTagsAdder(12)]
+        );
+    }
+
+    public function it_throws_if_customer_is_not_an_implementation_of_active_campaign_aware_interface(
+        CustomerRepositoryInterface $customerRepository,
+        SyliusCustomerInterface $syliusCustomer
+    ): void {
+        $customerRepository->find(12)->shouldBeCalledOnce()->willReturn($syliusCustomer);
+
+        $this->shouldThrow(new InvalidArgumentException('The Customer entity should implement the "Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface" class.'))->during(
+            '__invoke',
+            [new ContactTagsAdder(12)]
+        );
+    }
+
+    public function it_throws_if_customer_has_not_been_exported_to_active_campaign_yet(
+        ActiveCampaignAwareInterface $customer
+    ): void {
+        $customer->getActiveCampaignId()->willReturn(null);
+
+        $this->shouldThrow(new InvalidArgumentException('The Customer with id "12" does not have an ActiveCampaign id.'))->during(
+            '__invoke',
+            [new ContactTagsAdder(12)]
+        );
+    }
+}
---
+++ src/MessageHandler/Contact/ContactTagsAdderHandler.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact;
+
+use InvalidArgumentException;
+use Sylius\Component\Core\Model\CustomerInterface;
+use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactTagsAdder;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+
+final class ContactTagsAdderHandler
+{
+    public function __construct(
+        private ActiveCampaignResourceClientInterface $activeCampaignTagClient,
+        private ActiveCampaignResourceClientInterface $activeCampaignContactTagClient,
+        private CustomerRepositoryInterface $customerRepository
+    ) {
+    }
+
+    public function __invoke(ContactTagsAdder $message): void
+    {
+        $customerId = $message->getCustomerId();
+        /** @var CustomerInterface|null $customer */
+        $customer = $this->customerRepository->find($customerId);
+        if ($customer === null) {
+            throw new InvalidArgumentException(sprintf('Customer with id "%s" does not exists.', $customerId));
+        }
+        if (!$customer instanceof ActiveCampaignAwareInterface) {
+            throw new InvalidArgumentException(sprintf('The Customer entity should implement the "%s" class.', ActiveCampaignAwareInterface::class));
+        }
+        $activeCampaignId = $customer->getActiveCampaignId();
+        if ($activeCampaignId === null) {
+            throw new InvalidArgumentException(sprintf('The Customer with id "%s" does not have an ActiveCampaign id.', $customerId));
+        }
+    }
+}
