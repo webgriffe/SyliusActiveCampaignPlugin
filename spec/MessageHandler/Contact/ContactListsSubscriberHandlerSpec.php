9e2dda58418667cb4efc02ab2cebfa657e7bab9f
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-15 11:34:13 +0200

    Update contact list subscription (#41)

--- spec/MessageHandler/Contact/ContactListsSubscriberHandlerSpec.php
+++ spec/MessageHandler/Contact/ContactListsSubscriberHandlerSpec.php
@@ -7,15 +7,22 @@ namespace spec\Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact;
 use App\Entity\Channel\ChannelInterface;
 use App\Entity\Customer\CustomerInterface;
 use InvalidArgumentException;
+use Psr\Log\LoggerInterface;
 use Sylius\Component\Core\Model\ChannelInterface as SyliusChannelInterface;
 use Sylius\Component\Core\Model\CustomerInterface as SyliusCustomerInterface;
 use Sylius\Component\Core\Repository\CustomerRepositoryInterface;
+use Symfony\Component\HttpKernel\Exception\HttpException;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Mapper\ContactListMapperInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactListsSubscriber;
 use Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Contact\ContactListsSubscriberHandler;
 use PhpSpec\ObjectBehavior;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ContactListInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ChannelCustomerInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Resolver\CustomerChannelsResolverInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Resolver\ListSubscriptionStatusResolverInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\CreateResourceResponseInterface;
 
 class ContactListsSubscriberHandlerSpec extends ObjectBehavior
 {
@@ -23,15 +30,36 @@ class ContactListsSubscriberHandlerSpec extends ObjectBehavior
         CustomerRepositoryInterface $customerRepository,
         CustomerChannelsResolverInterface $customerChannelsResolver,
         ListSubscriptionStatusResolverInterface $listSubscriptionStatusResolver,
+        ActiveCampaignResourceClientInterface $activeCampaignContactListClient,
+        ContactListMapperInterface $contactListMapper,
+        LoggerInterface $logger,
         CustomerInterface $customer,
         ChannelInterface $firstChannel,
         SyliusChannelInterface $secondChannel,
+        ChannelInterface $thirdChannel,
+        ChannelInterface $fourthChannel,
+        ContactListInterface $firstContactList,
+        ContactListInterface $fourthContactList,
+        CreateResourceResponseInterface $createResourceResponse
     ): void {
         $customer->getActiveCampaignId()->willReturn(134);
         $customerRepository->find(12)->willReturn($customer);
-        $customerChannelsResolver->resolve($customer)->willReturn([$firstChannel, $secondChannel]);
+        $customerChannelsResolver->resolve($customer)->willReturn([$firstChannel, $secondChannel, $thirdChannel, $fourthChannel]);
 
-        $this->beConstructedWith($customerRepository, $customerChannelsResolver, $listSubscriptionStatusResolver);
+        $listSubscriptionStatusResolver->resolve($customer, $firstChannel)->willReturn(true);
+        $listSubscriptionStatusResolver->resolve($customer, $fourthChannel)->willReturn(false);
+
+        $firstChannel->getActiveCampaignListId()->willReturn(23);
+        $thirdChannel->getActiveCampaignListId()->willReturn(null);
+        $fourthChannel->getActiveCampaignListId()->willReturn(18);
+
+        $contactListMapper->mapFromListContactStatusAndSourceId(23, 134, ChannelCustomerInterface::SUBSCRIBED_TO_CONTACT_LIST)->willReturn($firstContactList);
+        $contactListMapper->mapFromListContactStatusAndSourceId(18, 134, ChannelCustomerInterface::UNSUBSCRIBED_FROM_CONTACT_LIST)->willReturn($fourthContactList);
+
+        $activeCampaignContactListClient->create($firstContactList)->willReturn($createResourceResponse);
+        $activeCampaignContactListClient->create($fourthContactList)->willThrow(new HttpException(200));
+
+        $this->beConstructedWith($customerRepository, $customerChannelsResolver, $listSubscriptionStatusResolver, $activeCampaignContactListClient, $contactListMapper, $logger);
     }
 
     public function it_is_initializable(): void
@@ -73,14 +101,22 @@ class ContactListsSubscriberHandlerSpec extends ObjectBehavior
         );
     }
 
-    public function it_handles_customer_list_subscriptions(
-        CustomerInterface $customer,
-        ChannelInterface $firstChannel,
-        SyliusChannelInterface $secondChannel,
-        ListSubscriptionStatusResolverInterface $listSubscriptionStatusResolver,
+    public function it_throws_if_http_exception_does_not_have_200_as_status_code(
+        ActiveCampaignResourceClientInterface $activeCampaignContactListClient,
+        ContactListInterface $fourthContactList
     ): void {
-        $listSubscriptionStatusResolver->resolve($customer, $firstChannel)->shouldBeCalledOnce()->willReturn(true);
-        $listSubscriptionStatusResolver->resolve($customer, $secondChannel)->shouldNotBeCalled();
+        $activeCampaignContactListClient->create($fourthContactList)->willThrow(new HttpException(400));
+
+        $this->shouldThrow(new HttpException(400))->during(
+            '__invoke',
+            [new ContactListsSubscriber(12)]
+        );
+    }
+
+    public function it_handles_customer_list_subscriptions(
+        LoggerInterface $logger
+    ): void {
+        $logger->info('The association with the list with id "18" already exists for the contact with id "134".')->shouldBeCalledOnce();
 
         $this->__invoke(new ContactListsSubscriber(12));
     }
