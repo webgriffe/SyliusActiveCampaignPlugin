a5821f07765b96acc5f888a522706d38553a119c
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-19 16:26:14 +0200

    Add Webhook Enqueuer (#44)

---
+++ spec/Enqueuer/WebhookEnqueuerSpec.php
@@ -0,0 +1,96 @@
+<?php
+
+declare(strict_types=1);
+
+namespace spec\Webgriffe\SyliusActiveCampaignPlugin\Enqueuer;
+
+use App\Entity\Channel\ChannelInterface;
+use Prophecy\Argument;
+use Symfony\Component\Messenger\Envelope;
+use Symfony\Component\Messenger\MessageBusInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\WebhookEnqueuer;
+use PhpSpec\ObjectBehavior;
+use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\WebhookEnqueuerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Generator\ChannelHostnameUrlGeneratorInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Webhook\WebhookCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\ListResourcesResponseInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\Webhook\WebhookResponse;
+use Webmozart\Assert\InvalidArgumentException;
+
+class WebhookEnqueuerSpec extends ObjectBehavior
+{
+    public function let(
+        MessageBusInterface $messageBus,
+        ActiveCampaignResourceClientInterface $activeCampaignWebhookClient,
+        ChannelHostnameUrlGeneratorInterface $channelHostnameUrlGenerator,
+        ChannelInterface $channel
+    ): void {
+        $channel->getId()->willReturn(3);
+        $channel->getActiveCampaignListId()->willReturn(4);
+
+        $channelHostnameUrlGenerator->generate($channel, 'TODO')->willReturn('https://localhost/webhook');
+
+        $this->beConstructedWith($messageBus, $activeCampaignWebhookClient, $channelHostnameUrlGenerator);
+    }
+
+    public function it_is_initializable(): void
+    {
+        $this->shouldBeAnInstanceOf(WebhookEnqueuer::class);
+    }
+
+    public function it_implements_webhook_enqueuer_interface(): void
+    {
+        $this->shouldImplement(WebhookEnqueuerInterface::class);
+    }
+
+    public function it_throws_if_channel_id_is_null(ChannelInterface $channel): void
+    {
+        $channel->getId()->willReturn(null);
+        $this->shouldThrow(new InvalidArgumentException('The channel id should not be null.'))
+            ->during('enqueue', [$channel]);
+    }
+
+    public function it_throws_if_channel_list_id_is_null(ChannelInterface $channel): void
+    {
+        $channel->getActiveCampaignListId()->willReturn(null);
+        $this->shouldThrow(new InvalidArgumentException('The channel ActiveCampaign list id should not be null.'))
+            ->during('enqueue', [$channel]);
+    }
+
+    public function it_does_not_enqueue_a_webhook_creation_if_it_is_already_found_on_active_campaign(
+        ChannelInterface $channel,
+        MessageBusInterface $messageBus,
+        ActiveCampaignResourceClientInterface $activeCampaignWebhookClient,
+        ListResourcesResponseInterface $listWebhooksResponse
+    ): void {
+        $activeCampaignWebhookClient->list([
+            'filters[url]' => 'https://localhost/webhook',
+            'filters[listid]' => 4,
+        ])->shouldBeCalledOnce()->willReturn($listWebhooksResponse);
+
+        $listWebhooksResponse->getResourceResponseLists()->shouldBeCalledOnce()->willReturn([new WebhookResponse(3)]);
+
+        $messageBus->dispatch(Argument::any())->shouldNotBeCalled();
+
+        $this->enqueue($channel);
+    }
+
+    public function it_enqueues_a_webhook_creation_if_it_is_not_found_on_active_campaign(
+        ChannelInterface $channel,
+        MessageBusInterface $messageBus,
+        ActiveCampaignResourceClientInterface $activeCampaignWebhookClient,
+        ListResourcesResponseInterface $listWebhooksResponse
+    ): void {
+        $activeCampaignWebhookClient->list([
+            'filters[url]' => 'https://localhost/webhook',
+            'filters[listid]' => 4,
+        ])->shouldBeCalledOnce()->willReturn($listWebhooksResponse);
+
+        $listWebhooksResponse->getResourceResponseLists()->shouldBeCalledOnce()->willReturn([]);
+
+        $messageBus->dispatch(Argument::type(WebhookCreate::class))->shouldBeCalledOnce()->willReturn(new Envelope(new WebhookCreate(3)));
+
+        $this->enqueue($channel);
+    }
+}
---
+++ src/Enqueuer/WebhookEnqueuer.php
@@ -0,0 +1,39 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusActiveCampaignPlugin\Enqueuer;
+
+use Symfony\Component\Messenger\MessageBusInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Generator\ChannelHostnameUrlGeneratorInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Webhook\WebhookCreate;
+use Webmozart\Assert\Assert;
+
+final class WebhookEnqueuer implements WebhookEnqueuerInterface
+{
+    public function __construct(
+        private MessageBusInterface $messageBus,
+        private ActiveCampaignResourceClientInterface $activeCampaignWebhookClient,
+        private ChannelHostnameUrlGeneratorInterface $channelHostnameUrlGenerator
+    ) {
+    }
+
+    public function enqueue($channel): void
+    {
+        /** @var string|int|null $channelId */
+        $channelId = $channel->getId();
+        Assert::notNull($channelId, 'The channel id should not be null.');
+        $activeCampaignListId = $channel->getActiveCampaignListId();
+        Assert::notNull($activeCampaignListId, 'The channel ActiveCampaign list id should not be null.');
+        $searchWebhooks = $this->activeCampaignWebhookClient->list([
+            'filters[url]' => $this->channelHostnameUrlGenerator->generate($channel, 'TODO'),
+            'filters[listid]' => (string) $activeCampaignListId,
+        ])->getResourceResponseLists();
+        if (count($searchWebhooks) > 0) {
+            return;
+        }
+
+        $this->messageBus->dispatch(new WebhookCreate($channelId));
+    }
+}
