5179ba515b839bc7f28ac49fbb0c24950b5d8746
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-19 10:41:03 +0200

    Use Connection Factory in Mapper (#46)

--- src/Mapper/ConnectionMapper.php
+++ src/Mapper/ConnectionMapper.php
@@ -5,18 +5,23 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusActiveCampaignPlugin\Mapper;
 
 use Sylius\Component\Core\Model\ChannelInterface;
-use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\Connection;
+use Webgriffe\SyliusActiveCampaignPlugin\Factory\ActiveCampaign\ConnectionFactoryInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign\ConnectionInterface;
 use Webmozart\Assert\Assert;
 
 final class ConnectionMapper implements ConnectionMapperInterface
 {
+    public function __construct(
+        private ConnectionFactoryInterface $connectionFactory
+    ) {
+    }
+
     public function mapFromChannel(ChannelInterface $channel): ConnectionInterface
     {
         $channelCode = $channel->getCode();
         Assert::notNull($channelCode, 'The channel does not have a code.');
 
-        return new Connection(
+        return $this->connectionFactory->createNew(
             'sylius',
             $channelCode,
             $channel->getName() ?? 'Sylius eCommerce'
