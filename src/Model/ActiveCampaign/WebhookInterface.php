8ac711dd3254bc72da08a341af4b60bb33ab2de1
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-19 12:22:56 +0200

    Add Webhook model (#44)

---
+++ src/Model/ActiveCampaign/WebhookInterface.php
@@ -0,0 +1,32 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaign;
+
+interface WebhookInterface
+{
+    public function getName(): string;
+
+    public function setName(string $name): void;
+
+    public function getUrl(): string;
+
+    public function setUrl(string $url): void;
+
+    /** @return string[] */
+    public function getEvents(): array;
+
+    /** @param string[] $events */
+    public function setEvents(array $events): void;
+
+    /** @return string[] */
+    public function getSources(): array;
+
+    /** @param string[] $sources */
+    public function setSources(array $sources): void;
+
+    public function getListId(): ?int;
+
+    public function setListId(?int $listId): void;
+}
