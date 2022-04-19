0c87424110853467ac8c104361e763ef4d4bc8b3
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-19 15:55:58 +0200

    Add Webhook create handler (#44)

---
+++ src/MessageHandler/Webhook/WebhookCreateHandler.php
@@ -0,0 +1,49 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusActiveCampaignPlugin\MessageHandler\Webhook;
+
+use InvalidArgumentException;
+use Sylius\Component\Channel\Repository\ChannelRepositoryInterface;
+use Sylius\Component\Core\Model\ChannelInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Client\ActiveCampaignResourceClientInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Generator\ChannelHostnameUrlGeneratorInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Mapper\WebhookMapperInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Webhook\WebhookCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ChannelActiveCampaignAwareInterface;
+
+final class WebhookCreateHandler
+{
+    public function __construct(
+        private WebhookMapperInterface $webhookMapper,
+        private ActiveCampaignResourceClientInterface $activeCampaignWebhookClient,
+        private ChannelRepositoryInterface $channelRepository,
+        private ChannelHostnameUrlGeneratorInterface $channelHostnameUrlGenerator
+    ) {
+    }
+
+    public function __invoke(WebhookCreate $message): void
+    {
+        $channelId = $message->getChannelId();
+        /** @var ChannelInterface|null $channel */
+        $channel = $this->channelRepository->find($channelId);
+        if ($channel === null) {
+            throw new InvalidArgumentException(sprintf('Channel with id "%s" does not exists.', $channelId));
+        }
+        if (!$channel instanceof ChannelActiveCampaignAwareInterface) {
+            throw new InvalidArgumentException(sprintf('The Channel entity should implement the "%s" class.', ChannelActiveCampaignAwareInterface::class));
+        }
+        $activeCampaignListId = $channel->getActiveCampaignListId();
+        if ($activeCampaignListId === null) {
+            throw new InvalidArgumentException(sprintf('The Channel with id "%s" does not have an ActiveCampaign list id.', $channelId));
+        }
+        $this->activeCampaignWebhookClient->create($this->webhookMapper->map(
+            sprintf('Update Sylius newsletter subscription to list "%s"', $activeCampaignListId),
+            $this->channelHostnameUrlGenerator->generate($channel, 'TODO'),
+            ['subscribe', 'unsubscribe'],
+            ['public', 'admin', 'system'],
+            $activeCampaignListId
+        ));
+    }
+}
