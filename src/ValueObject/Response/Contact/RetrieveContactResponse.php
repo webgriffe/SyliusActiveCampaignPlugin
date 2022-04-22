5e310a6903b89352c63fb6b8c150e267420516f7
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2022-04-22 16:43:08 +0200

    Ensure of the contact lists structure (#44)

--- src/ValueObject/Response/Contact/RetrieveContactResponse.php
+++ src/ValueObject/Response/Contact/RetrieveContactResponse.php
@@ -8,11 +8,26 @@ use Webgriffe\SyliusActiveCampaignPlugin\ValueObject\Response\ResourceResponseIn
 
 final class RetrieveContactResponse implements RetrieveContactResponseInterface
 {
-    /** @param array<array-key, array{contact: string, list: string, status: string, id: string}> $contactLists */
+    /** @var array<array-key, array{contact: string, list: string, status: string, id: string}> */
+    private array $contactLists = [];
+
+    /** @param array<array-key, array> $contactLists */
     public function __construct(
         private ContactResponse $contact,
-        private array $contactLists
+        array $contactLists
     ) {
+        // @TODO This should be replaced by serializing this array of array as an array of objects
+        /** @var array{contact: string, list: string, status: string, id: string}|array<string, string> $contactList */
+        foreach ($contactLists as $contactList) {
+            if (!array_key_exists('contact', $contactList) ||
+                !array_key_exists('list', $contactList) ||
+                !array_key_exists('status', $contactList) ||
+                !array_key_exists('id', $contactList)
+            ) {
+                continue;
+            }
+            $this->contactLists[] = $contactList;
+        }
     }
 
     public function getResourceResponse(): ResourceResponseInterface
