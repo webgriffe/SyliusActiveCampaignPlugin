9a1d594c467211caaf404c67d4479e08d2c2764f
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-06-05 12:18:41 +0200

    Enqueue also contact and relatives after order event (#67)

--- src/EventSubscriber/OrderSubscriber.php
+++ src/EventSubscriber/OrderSubscriber.php
@@ -5,14 +5,21 @@ declare(strict_types=1);
 namespace Webgriffe\SyliusActiveCampaignPlugin\EventSubscriber;
 
 use Psr\Log\LoggerInterface;
+use Sylius\Component\Core\Model\CustomerInterface;
 use Sylius\Component\Core\Model\OrderInterface;
 use Symfony\Component\EventDispatcher\EventSubscriberInterface;
 use Symfony\Component\EventDispatcher\GenericEvent;
 use Symfony\Component\Messenger\MessageBusInterface;
 use Throwable;
+use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\ContactEnqueuerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\EcommerceCustomerEnqueuerInterface;
 use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\EcommerceOrderEnqueuerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactListsSubscriber;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactTagsAdder;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderRemove;
 use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\CustomerActiveCampaignAwareInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Resolver\CustomerChannelsResolverInterface;
 
 final class OrderSubscriber implements EventSubscriberInterface
 {
@@ -20,17 +27,107 @@ final class OrderSubscriber implements EventSubscriberInterface
         private MessageBusInterface $messageBus,
         private EcommerceOrderEnqueuerInterface $ecommerceOrderEnqueuer,
         private LoggerInterface $logger,
+        private ContactEnqueuerInterface $contactEnqueuer,
+        private EcommerceCustomerEnqueuerInterface $ecommerceCustomerEnqueuer,
+        private CustomerChannelsResolverInterface $customerChannelsResolver,
     ) {
     }
 
     public static function getSubscribedEvents(): array
     {
         return [
-            'sylius.order.post_complete' => ['enqueueOrderInRealTime'],
-            'sylius.order.post_create' => ['enqueueOrderNotInRealTime'],
-            'sylius.order.post_update' => ['enqueueOrderNotInRealTime'],
+            'sylius.order.post_complete' => [['enqueueContact'], ['enqueueEcommerceCustomer'], ['addContactTags'], ['subscribeContactToLists'], ['enqueueOrderInRealTime']],
+            'sylius.order.post_create' => [['enqueueContact'], ['enqueueEcommerceCustomer'], ['addContactTags'], ['subscribeContactToLists'], ['enqueueOrderNotInRealTime']],
+            'sylius.order.post_update' => [['enqueueContact'], ['enqueueEcommerceCustomer'], ['addContactTags'], ['subscribeContactToLists'], ['enqueueOrderNotInRealTime']],
             'sylius.order.post_delete' => ['removeOrder'],
         ];
+    }
+
+    public function enqueueContact(GenericEvent $event): void
+    {
+        $order = $event->getSubject();
+        if (!$order instanceof OrderInterface || !$order instanceof ActiveCampaignAwareInterface || $order->getCustomer() === null) {
+            return;
+        }
+        $customer = $order->getCustomer();
+        if (!$customer instanceof CustomerInterface || !$customer instanceof CustomerActiveCampaignAwareInterface) {
+            return;
+        }
+
+        try {
+            $this->contactEnqueuer->enqueue($customer);
+        } catch (Throwable $throwable) {
+            $this->logger->error($throwable->getMessage(), $throwable->getTrace());
+        }
+    }
+
+    public function enqueueEcommerceCustomer(GenericEvent $event): void
+    {
+        $order = $event->getSubject();
+        if (!$order instanceof OrderInterface || !$order instanceof ActiveCampaignAwareInterface || $order->getCustomer() === null) {
+            return;
+        }
+        $customer = $order->getCustomer();
+        if (!$customer instanceof CustomerInterface || !$customer instanceof CustomerActiveCampaignAwareInterface) {
+            return;
+        }
+        foreach ($this->customerChannelsResolver->resolve($customer) as $channel) {
+            if (!$channel instanceof ActiveCampaignAwareInterface) {
+                return;
+            }
+
+            try {
+                $this->ecommerceCustomerEnqueuer->enqueue($customer, $channel);
+            } catch (Throwable $throwable) {
+                $this->logger->error($throwable->getMessage(), $throwable->getTrace());
+            }
+        }
+    }
+
+    public function addContactTags(GenericEvent $event): void
+    {
+        $order = $event->getSubject();
+        if (!$order instanceof OrderInterface || !$order instanceof ActiveCampaignAwareInterface || $order->getCustomer() === null) {
+            return;
+        }
+        $customer = $order->getCustomer();
+        if (!$customer instanceof CustomerInterface || !$customer instanceof CustomerActiveCampaignAwareInterface) {
+            return;
+        }
+        /** @var int|string|null $customerId */
+        $customerId = $customer->getId();
+        if ($customerId === null) {
+            return;
+        }
+
+        try {
+            $this->messageBus->dispatch(new ContactTagsAdder($customerId));
+        } catch (Throwable $throwable) {
+            $this->logger->error($throwable->getMessage(), $throwable->getTrace());
+        }
+    }
+
+    public function subscribeContactToLists(GenericEvent $event): void
+    {
+        $order = $event->getSubject();
+        if (!$order instanceof OrderInterface || !$order instanceof ActiveCampaignAwareInterface || $order->getCustomer() === null) {
+            return;
+        }
+        $customer = $order->getCustomer();
+        if (!$customer instanceof CustomerInterface || !$customer instanceof CustomerActiveCampaignAwareInterface) {
+            return;
+        }
+        /** @var int|string|null $customerId */
+        $customerId = $customer->getId();
+        if ($customerId === null) {
+            return;
+        }
+
+        try {
+            $this->messageBus->dispatch(new ContactListsSubscriber($customerId));
+        } catch (Throwable $throwable) {
+            $this->logger->error($throwable->getMessage(), $throwable->getTrace());
+        }
     }
 
     public function enqueueOrderInRealTime(GenericEvent $event): void
