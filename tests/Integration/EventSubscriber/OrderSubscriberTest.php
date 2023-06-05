03f1e9a9db887a3d652e36919cb9d302efcba90f
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-06-05 12:18:41 +0200

    Enqueue also contact and relatives after order event (#67)

--- tests/Integration/EventSubscriber/OrderSubscriberTest.php
+++ tests/Integration/EventSubscriber/OrderSubscriberTest.php
@@ -9,6 +9,10 @@ use Sylius\Bundle\ResourceBundle\Event\ResourceControllerEvent;
 use Sylius\Component\Core\Repository\OrderRepositoryInterface;
 use Symfony\Component\Messenger\Envelope;
 use Symfony\Component\Messenger\Transport\InMemoryTransport;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactCreate;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactListsSubscriber;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactTagsAdder;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceCustomer\EcommerceCustomerCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderCreate;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderRemove;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\EcommerceOrder\EcommerceOrderUpdate;
@@ -32,7 +36,7 @@ final class OrderSubscriberTest extends AbstractEventDispatcherTest
         ], [], [], PurgeMode::createDeleteMode());
     }
 
-    public function test_that_it_creates_ecommerce_order_on_active_campaign(): void
+    public function test_that_it_creates_contact_and_relatives_and_ecommerce_order_on_active_campaign_on_order_creation(): void
     {
         $order = $this->orderRepository->findOneBy(['number' => '0001']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($order), 'sylius.order.post_create');
@@ -40,14 +44,32 @@ final class OrderSubscriberTest extends AbstractEventDispatcherTest
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+        $this->assertCount(5, $messages);
+
         $message = $messages[0];
+        $this->assertInstanceOf(ContactCreate::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerCreate::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($order->getChannel()->getId(), $message->getMessage()->getChannelId());
+
+        $message = $messages[2];
+        $this->assertInstanceOf(ContactTagsAdder::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[3];
+        $this->assertInstanceOf(ContactListsSubscriber::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[4];
         $this->assertInstanceOf(EcommerceOrderCreate::class, $message->getMessage());
         $this->assertEquals($order->getId(), $message->getMessage()->getOrderId());
         $this->assertFalse($message->getMessage()->isInRealTime());
     }
 
-    public function test_that_it_updates_ecommerce_order_on_active_campaign(): void
+    public function test_that_it_updates_contact_and_relatives_and_ecommerce_order_on_active_campaign_after_order_update(): void
     {
         $order = $this->orderRepository->findOneBy(['number' => '0002']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($order), 'sylius.order.post_update');
@@ -55,8 +77,26 @@ final class OrderSubscriberTest extends AbstractEventDispatcherTest
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+        $this->assertCount(5, $messages);
+
         $message = $messages[0];
+        $this->assertInstanceOf(ContactCreate::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerCreate::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($order->getChannel()->getId(), $message->getMessage()->getChannelId());
+
+        $message = $messages[2];
+        $this->assertInstanceOf(ContactTagsAdder::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[3];
+        $this->assertInstanceOf(ContactListsSubscriber::class, $message->getMessage());
+        $this->assertEquals($order->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[4];
         $this->assertInstanceOf(EcommerceOrderUpdate::class, $message->getMessage());
         $this->assertEquals($order->getId(), $message->getMessage()->getOrderId());
         $this->assertEquals($order->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
@@ -77,7 +117,7 @@ final class OrderSubscriberTest extends AbstractEventDispatcherTest
         $this->assertEquals($order->getActiveCampaignId(), $message->getMessage()->getActiveCampaignId());
     }
 
-    public function test_that_it_creates_ecommerce_order_on_active_campaign_after_checkout_complete(): void
+    public function test_that_it_creates_contact_and_relatives_and_ecommerce_order_on_active_campaign_after_checkout_complete(): void
     {
         $newOrder = $this->orderRepository->findOneBy(['number' => '0001']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($newOrder), 'sylius.order.post_complete');
@@ -85,14 +125,32 @@ final class OrderSubscriberTest extends AbstractEventDispatcherTest
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+        $this->assertCount(5, $messages);
+
         $message = $messages[0];
+        $this->assertInstanceOf(ContactCreate::class, $message->getMessage());
+        $this->assertEquals($newOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerCreate::class, $message->getMessage());
+        $this->assertEquals($newOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($newOrder->getChannel()->getId(), $message->getMessage()->getChannelId());
+
+        $message = $messages[2];
+        $this->assertInstanceOf(ContactTagsAdder::class, $message->getMessage());
+        $this->assertEquals($newOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[3];
+        $this->assertInstanceOf(ContactListsSubscriber::class, $message->getMessage());
+        $this->assertEquals($newOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[4];
         $this->assertInstanceOf(EcommerceOrderCreate::class, $message->getMessage());
         $this->assertEquals($newOrder->getId(), $message->getMessage()->getOrderId());
         $this->assertTrue($message->getMessage()->isInRealTime());
     }
 
-    public function test_that_it_updates_ecommerce_order_on_active_campaign_after_checkout_complete(): void
+    public function test_that_it_updates_contact_and_relatives_and_ecommerce_order_on_active_campaign_after_checkout_complete(): void
     {
         $oldOrder = $this->orderRepository->findOneBy(['number' => '0002']);
         $this->eventDispatcher->dispatch(new ResourceControllerEvent($oldOrder), 'sylius.order.post_complete');
@@ -100,8 +158,27 @@ final class OrderSubscriberTest extends AbstractEventDispatcherTest
         $transport = self::getContainer()->get('messenger.transport.main');
         /** @var Envelope[] $messages */
         $messages = $transport->get();
-        $this->assertCount(1, $messages);
+
+        $this->assertCount(5, $messages);
+
         $message = $messages[0];
+        $this->assertInstanceOf(ContactCreate::class, $message->getMessage());
+        $this->assertEquals($oldOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[1];
+        $this->assertInstanceOf(EcommerceCustomerCreate::class, $message->getMessage());
+        $this->assertEquals($oldOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+        $this->assertEquals($oldOrder->getChannel()->getId(), $message->getMessage()->getChannelId());
+
+        $message = $messages[2];
+        $this->assertInstanceOf(ContactTagsAdder::class, $message->getMessage());
+        $this->assertEquals($oldOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[3];
+        $this->assertInstanceOf(ContactListsSubscriber::class, $message->getMessage());
+        $this->assertEquals($oldOrder->getCustomer()->getId(), $message->getMessage()->getCustomerId());
+
+        $message = $messages[4];
         $this->assertInstanceOf(EcommerceOrderUpdate::class, $message->getMessage());
         $this->assertEquals($oldOrder->getId(), $message->getMessage()->getOrderId());
         $this->assertTrue($message->getMessage()->isInRealTime());
