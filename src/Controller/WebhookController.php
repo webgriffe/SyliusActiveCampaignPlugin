80f4fb27e02b1f78d6e9afe4d4e1f1c58539db4f
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2023-08-30 17:27:21 +0200

    Dispatch contact automation event from AC

--- src/Controller/WebhookController.php
+++ src/Controller/WebhookController.php
@@ -9,6 +9,7 @@ use Symfony\Component\HttpFoundation\Exception\BadRequestException;
 use Symfony\Component\HttpFoundation\Request;
 use Symfony\Component\HttpFoundation\Response;
 use Symfony\Component\Messenger\MessageBusInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactAutomationEvent;
 use Webgriffe\SyliusActiveCampaignPlugin\Message\Contact\ContactListsUpdater;
 use Webgriffe\SyliusActiveCampaignPlugin\Repository\ActiveCampaignCustomerRepositoryInterface;
 use Webmozart\Assert\Assert;
@@ -42,6 +43,26 @@ final class WebhookController extends AbstractController
         Assert::notNull($customerId);
 
         $this->messageBus->dispatch(new ContactListsUpdater($customerId));
+
+        return new Response('OK');
+    }
+
+    public function dispatchContactAutomationEventAction(Request $request): Response
+    {
+        /** @var array{"id":string, "email":string,"first_name":string,"last_name":string,"phone":string,"ip4":string,"tags":string,"customer_acct_name":string,"orgname":string} $contact */
+        $contact = $request->request->all('contact');
+        $seriesId = $request->request->get('seriesid');
+
+        $customer = $this->customerRepository->findByContactId((int) $contact['id']);
+        if ($customer === null) {
+            return new Response('Customer for contact not found.');
+        }
+        /** @var string|int|null $customerId */
+        $customerId = $customer->getId();
+        Assert::notNull($customerId);
+        Assert::string($seriesId);
+
+        $this->messageBus->dispatch(new ContactAutomationEvent($customerId, $seriesId));
 
         return new Response('OK');
     }
