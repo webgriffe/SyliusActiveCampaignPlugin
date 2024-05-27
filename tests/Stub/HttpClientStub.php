531302a6b50da7fcec27789f29b0c31a9eec1662
Lorenzo Ruozzi <lruozzi@webgriffe.com>
2024-05-27 11:54:47 +0200

    Support for Sylius 1.13

--- tests/Stub/HttpClientStub.php
+++ tests/Stub/HttpClientStub.php
@@ -5,8 +5,10 @@ declare(strict_types=1);
 namespace Tests\Webgriffe\SyliusActiveCampaignPlugin\Stub;
 
 use GuzzleHttp\ClientInterface;
+use GuzzleHttp\Promise\PromiseInterface;
 use GuzzleHttp\Psr7\Response;
 use Psr\Http\Message\RequestInterface;
+use Psr\Http\Message\ResponseInterface;
 use RuntimeException;
 
 final class HttpClientStub implements ClientInterface
@@ -23,29 +25,29 @@ final class HttpClientStub implements ClientInterface
         self::$sentRequests = [];
     }
 
-    public function send(RequestInterface $request, array $options = [])
+    public function send(RequestInterface $request, array $options = []): ResponseInterface
     {
         self::$sentRequests[] = $request;
 
         return new Response(self::$responseStatusCode, [], self::$responseBodyContent);
     }
 
-    public function sendAsync(RequestInterface $request, array $options = [])
+    public function sendAsync(RequestInterface $request, array $options = []): PromiseInterface
     {
         throw new RuntimeException('Not implemented');
     }
 
-    public function request($method, $uri, array $options = [])
+    public function request($method, $uri, array $options = []): ResponseInterface
     {
         throw new RuntimeException('Not implemented');
     }
 
-    public function requestAsync($method, $uri, array $options = [])
+    public function requestAsync($method, $uri, array $options = []): PromiseInterface
     {
         throw new RuntimeException('Not implemented');
     }
 
-    public function getConfig($option = null)
+    public function getConfig($option = null): void
     {
         throw new RuntimeException('Not implemented');
     }
