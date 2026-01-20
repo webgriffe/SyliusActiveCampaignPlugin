a3e2f85e5d5f4ccee95a0a79049a22d41e2648cc
Luca Gallinari <luke.gallinari@gmail.com>
2026-01-20 11:26:09 +0100

    Migrate to Symfony Workflow

---
+++ src/EventSubscriber/OrderPaymentWorkflowSubscriber.php
@@ -0,0 +1,43 @@
+<?php
+
+declare(strict_types=1);
+
+namespace Webgriffe\SyliusActiveCampaignPlugin\EventSubscriber;
+
+use Sylius\Component\Core\Model\OrderInterface;
+use Sylius\Component\Core\OrderPaymentTransitions;
+use Symfony\Component\EventDispatcher\EventSubscriberInterface;
+use Symfony\Component\Workflow\Event\CompletedEvent;
+use Webgriffe\SyliusActiveCampaignPlugin\Enqueuer\RealTimeOrderEnqueuerInterface;
+use Webgriffe\SyliusActiveCampaignPlugin\Model\ActiveCampaignAwareInterface;
+
+final class OrderPaymentWorkflowSubscriber implements EventSubscriberInterface
+{
+    public function __construct(
+        private readonly RealTimeOrderEnqueuerInterface $realTimeOrderEnqueuer,
+    ) {
+    }
+
+    public static function getSubscribedEvents(): array
+    {
+        return [
+            sprintf(
+                'workflow.%s.completed.%s',
+                OrderPaymentTransitions::GRAPH,
+                OrderPaymentTransitions::TRANSITION_PAY,
+            ) => ['onOrderPaymentPaid', 400],
+        ];
+    }
+
+    public function onOrderPaymentPaid(CompletedEvent $event): void
+    {
+        /** @var OrderInterface&ActiveCampaignAwareInterface $order */
+        $order = $event->getSubject();
+
+        if (!$order instanceof OrderInterface || !$order instanceof ActiveCampaignAwareInterface) {
+            return;
+        }
+
+        $this->realTimeOrderEnqueuer->enqueue($order);
+    }
+}
